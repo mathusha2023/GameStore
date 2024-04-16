073fa6ee6e5349fd6b3b201fa4c28f3d9f49d355
Alexey <bazooka2008@inbox.ru>
2024-04-16 21:41:04 +0300

    api starting

--- api/api.py
+++ api/api.py
@@ -1,3 +1,15 @@
+from flask import Flask, jsonify
+from flask_restful import reqparse, abort, Api, Resource
 from data import db_session
 
-db_session.global_init("db/games.db")
+app = Flask(__name__)
+api = Api(app)
+
+
+def main():
+    db_session.global_init("db/games.db")
+    app.run(host="127.0.0.1", port=8080)
+
+
+if __name__ == "__main__":
+    main()
---
+++ api/game_resources.py
@@ -0,0 +1,4 @@
+from flask import make_response, jsonify
+from flask_restful import reqparse, abort, Api, Resource
+
+
