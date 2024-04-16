2b9d59044fbef32153c6d0539a0dee10cb7f73eb
MrLeviReaper <kocheevvadya@yandex.ru>
2024-04-16 21:30:16 +0300

    Попытки в базу зеров

---
+++ renderBack/data/users.py
@@ -0,0 +1,13 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    login = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True, index=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False, index=True)
+
--- renderBack/server.py
+++ renderBack/server.py
@@ -1,12 +1,30 @@
 from flask import Flask
+from data import db_session
+from data.users import User
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'niggers'
 
-
+def main():
+    db_session.global_init("db/users.db")
+    # app.run(host="127.0.0.1", port=8000)
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "Тута будет основной чето"
+@app.route('/register')
+def about():
+    return 'Здесь будет регистрация'
+@app.route('/login', methods=['GET', 'POST'])
+def about1():
+    return 'Здесь будет вход'
+# @app.route('/<>')
+# def about2():
+#     return 'Здесь будет информация об одной игре лол'
+@app.route('/about')
+def about3():
+    return 'Здесь будет информация об авторе сайта.'
+
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000)
+    main()
