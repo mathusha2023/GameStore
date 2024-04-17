cdde7b551453a15ae7fdae5bc7e39271c1ad937c
MrLeviReaper <kocheevvadya@yandex.ru>
2024-04-17 23:03:33 +0300

    Рабочая регистрация + Логин(вроде бы) + попытка сделать дизайн (провалившаяся в broken designer shit)

--- renderBack/server.py
+++ renderBack/server.py
@@ -1,21 +1,27 @@
 from flask import Flask, render_template, redirect
 from data import db_session
+import os
 from data.users import User
 from forms.registerform import RegisterForm
 from forms.loginform import LoginForm
+from flask_login import LoginManager, login_user
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'niggers'
 
 
 def main():
+    if not os.path.isdir("db"):
+        os.mkdir("db")
     db_session.global_init("db/users.db")
     app.run(host="127.0.0.1", port=8000)
 
 
 @app.route("/")
 def index():
-    return "Тута будет основной чето"
+    return render_template('index.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -43,16 +49,26 @@ def reqister():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/')
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.login == form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
     return render_template('login.html', title='Авторизация', form=form)
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
-# @app.route('/<>')
-# def about2():
-#     return 'Здесь будет информация об одной игре лол'
-@app.route('/about')
-def about3():
-    return 'Здесь будет информация об авторе сайта.'
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 if __name__ == "__main__":
