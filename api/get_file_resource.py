4296c69a9d9c112d06b40543f04160f433df0efb
Alexey <bazooka2008@inbox.ru>
2024-04-18 18:58:09 +0300

    доработка получения файла

--- api/get_file_resource.py
+++ api/get_file_resource.py
@@ -8,6 +8,11 @@ class GetFileResource(Resource):
         path = request.args.get("filepath", None)
         if path is None:
             abort(400, message="Filepath argument required!")
+        check_path = path.replace("\\", "/").split("/")
+        if len(check_path) < 2:
+            abort(404, message=f"File {path} not found")
+        if check_path[0] != "db" or check_path[1] != "games":
+            abort(403, message=f"Access to the file {path} is prohibited")
         if not os.path.isfile(path):
             abort(404, message=f"File {path} not found")
         return send_file(path)
