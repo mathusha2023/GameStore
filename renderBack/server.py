5e41e2ceee01b17ec51cb6afcd38ea61a98401cc
MrLeviReaper <kocheevvadya@yandex.ru>
2024-04-29 21:08:39 +0300

    Удаление всего

--- renderBack/server.py
+++ renderBack/server.py
@@ -4,6 +4,7 @@ from data.db_session import create_session
 from dotenv import load_dotenv
 import os
 from data.users import User
+from forms.commentform import ReviewForm
 from forms.registerform import RegisterForm
 from forms.loginform import LoginForm
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
@@ -160,7 +161,21 @@ def mygames():
     return render_template('mygames.html', game_dict=game_dict, api_url=api_url, title="Мои игры")
 
 
-@app.route('/game/<game_id>')
+@app.route('/game/<game_id>', methods=['POST', 'GET'])
+# def send_message(game_id, user_id, mark, message):
+#     url = f"{api_url}/comment/{game_id}"
+#     data = {
+#         "mark": mark,
+#         "user": user_id,
+#         "message": message
+#     }
+#     response = requests.put(url, json=data)
+#     if response.status_code == 200:
+#         print("Комментарий успешно отправлен")
+#     else:
+#         print("Ошибка при отправке комментария:", response.text)
+
+
 def game_detail(game_id):
     game_data = {}
     url = f'{api_url}/game/{game_id}'
@@ -172,14 +187,24 @@ def game_detail(game_id):
         if author:
             game_data['author'] = author
             game_data['author_id'] = author_id
-    print(game_data)
-    return render_template('game.html', game=game_data, api_url=api_url, title=game_data["title"])
+    form = ReviewForm(request.form)
+    # if request.method == 'POST' and form.validate():
+    #     mark = form.rating.data
+    #     message = form.review.data
+    #     user_id = current_user.get_id()
+    #     send_comment(game_id, user_id, mark, message)
+    #     return redirect(f"/game/{game_id}")
+    return render_template('game.html', game=game_data, form=form, api_url=api_url, title=game_data["title"])
 
 
 @app.route("/delete/<game_id>")
 def delete(game_id):
-    requests.delete(game_id)
-    return redirect('/index')
+    url = f"{api_url}/delete/{game_id}"
+    response = requests.delete(url)
+    if response.status_code == 200:
+        print("Игра успешно удалена")
+    else:
+        print("Ошибка при удалении игры:", response.text)
 
 
 @app.route("/privacy")
